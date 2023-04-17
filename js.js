62f4c9f2382479f5afe2c4d4e3fb09aebb4fcdb3
ark810454 <ark810454@gmail.com>
2023-04-17 10:36:53 +0100

    Initial commit

---
+++ js.js
@@ -0,0 +1,17 @@
+
+function videoPres(event){
+    var vid= document.querySelector("#vid")
+    var toggle= document.querySelector("#toggle")
+        vid.style.display="flex";
+        toggle.style.display="block";
+        
+}
+function close(event){
+    var vid= document.querySelector("#vid")
+        vid.style.display="none";
+        
+        
+}
+function firsttheme(){
+    var first= document.querySelector("#firsttheme")
+}
